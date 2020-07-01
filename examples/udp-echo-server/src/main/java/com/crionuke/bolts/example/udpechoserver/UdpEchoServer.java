8e90c40bf50a746d18ca8ad8373b365dfcb42f85
Kirill Byvshev <k@byv.sh>
2020-07-01 11:32:56 +0300

    clean main class

--- examples/udp-echo-server/src/main/java/com/crionuke/bolts/example/udpechoserver/UdpEchoServer.java
+++ examples/udp-echo-server/src/main/java/com/crionuke/bolts/example/udpechoserver/UdpEchoServer.java
@@ -2,10 +2,8 @@ package com.crionuke.bolts.example.udpechoserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.crionuke")
 public class UdpEchoServer {
 
     public static void main(String[] args) {
